209343a61c8583c5cb8a34db35311f91aad3db4c
plamenyovchev <plamenkooo@gmail.com>
2020-10-14 23:41:18 +0300

    Use Products component

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,12 @@ import Header from "./Header";
 import Spinner from "./Spinner";
 
 import useFetch from "./hooks/useFetch";
+import Products from "./components/Products/Products";
 
 export default function App() {
   const { data: products, loading, error } = useFetch(
     "products?category=shoes"
   );
-
-  const renderProduct = (p) => {
-    return (
-      <div key={p.id} className="product">
-        <a href="/">
-          <img src={`/images/${p.image}`} alt={p.name} />
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-        </a>
-      </div>
-    );
-  };
 
   if (error) {
     throw error;
@@ -46,7 +35,7 @@ export default function App() {
             </select>
           </section>
           <section id="products">
-            {products && products.map(renderProduct)}
+            <Products products={products} />
           </section>
         </main>
       </div>
