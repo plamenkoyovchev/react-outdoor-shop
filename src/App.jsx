55b03abd40e0f4f63ea781e2de3b9bec6e4479d4
plamenyovchev <plamenkooo@gmail.com>
2020-10-16 23:22:24 +0300

    Move products logic to Products component

--- src/App.jsx
+++ src/App.jsx
@@ -2,41 +2,15 @@ import React from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
-import Spinner from "./Spinner";
-
-import useFetch from "./hooks/useFetch";
 import Products from "./components/Products/Products";
 
 export default function App() {
-  const { data: products, loading, error } = useFetch(
-    "products?category=shoes"
-  );
-
-  if (error) {
-    throw error;
-  }
-
-  if (loading) {
-    return <Spinner />;
-  }
-
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <section id="filters">
-            <label htmlFor="size">Filter by Size:</label>
-            <select id="size">
-              <option value="">All sizes</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-            </select>
-          </section>
-          <section id="products">
-            <Products products={products} />
-          </section>
+          <Products />
         </main>
       </div>
       <Footer />
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,37 @@
 import React from "react";
+import useFetch from "../../hooks/useFetch";
+import Spinner from "../../Spinner";
 import Product from "../Product/Product";
 
-const Products = ({ products }) => {
-  return products && products.map((p) => <Product key={p.id} {...p} />);
+const Products = () => {
+  const { data: products, loading, error } = useFetch(
+    "products?category=shoes"
+  );
+
+  if (error) {
+    throw error;
+  }
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <section id="filters">
+        <label htmlFor="size">Filter by Size:</label>
+        <select id="size">
+          <option value="">All sizes</option>
+          <option value="7">7</option>
+          <option value="8">8</option>
+          <option value="9">9</option>
+        </select>
+      </section>
+      <section id="products">
+        {products && products.map((p) => <Product key={p.id} {...p} />)}
+      </section>
+    </>
+  );
 };
 
 export default Products;
