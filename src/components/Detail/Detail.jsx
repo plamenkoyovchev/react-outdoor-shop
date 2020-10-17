bd3dfcb8dbaeccabef654a3c3c82c6dd83d9314e
plamenyovchev <plamenkooo@gmail.com>
2020-10-17 14:13:11 +0300

    Navigate to cart

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "../../hooks/useFetch";
 import Spinner from "../../Spinner";
 import NotFound from "../NotFound/NotFound";
 
 const Detail = () => {
   const { id } = useParams();
+  const navigate = useNavigate();
   const { loading, error, data: product } = useFetch(`products/${id}`);
 
   if (loading) {
@@ -25,6 +26,11 @@ const Detail = () => {
       <h1>{product.name}</h1>
       <p>{product.description}</p>
       <p id="price">${product.price}</p>
+      <p>
+        <button className="btn btn-primary" onClick={() => navigate("/cart")}>
+          Add to Cart
+        </button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category} />
     </div>
   );
