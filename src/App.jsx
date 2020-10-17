6d89826a2e152e585a676b7627509e91caeba799
plamenyovchev <plamenkooo@gmail.com>
2020-10-17 08:43:07 +0300

    Define Routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,21 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Products from "./components/Products/Products";
 
+import { Routes, Route } from "react-router-dom";
+import Detail from "./components/Detail/Detail";
+import Cart from "./components/Cart/Cart";
+
 export default function App() {
   return (
     <>
       <div className="content">
         <Header />
         <main>
-          <Products />
+          <Routes>
+            <Route path="/" element={<Products />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/cart" element={<Cart />} />
+          </Routes>
         </main>
       </div>
       <Footer />
