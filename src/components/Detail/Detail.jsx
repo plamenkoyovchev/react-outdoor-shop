38f0b0038670c8ab29232930123432a2eac35800
plamenyovchev <plamenkooo@gmail.com>
2020-10-17 13:54:42 +0300

    Get details

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import Spinner from "../../Spinner";
+import NotFound from "../NotFound/NotFound";
 
 const Detail = () => {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  const { loading, error, data: product } = useFetch(`products/${id}`);
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  if (!product) {
+    return <NotFound />;
+  }
+
+  if (error) {
+    throw error;
+  }
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 };
 
 export default Detail;
