2e5c44ca24ede43199128cf3239857b76506a74e
plamenyovchev <plamenkooo@gmail.com>
2020-10-18 08:53:25 +0300

    Filter products by size

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useFetch from "../../hooks/useFetch";
 import Spinner from "../../Spinner";
 import Product from "../Product/Product";
@@ -10,6 +10,13 @@ const Products = () => {
   const { data: products, loading, error } = useFetch(
     `products?category=${category}`
   );
+  const [size, setSize] = useState("");
+
+  const getFilteredProducts = () => {
+    return size
+      ? products.filter((p) => p.skus.find((s) => s.size === parseInt(size)))
+      : products;
+  };
 
   if (error) {
     throw error;
@@ -23,20 +30,23 @@ const Products = () => {
     return <NotFound />;
   }
 
+  const filteredProducts = getFilteredProducts();
+
   return (
     <>
       <section id="filters">
         <label htmlFor="size">Filter by Size:</label>
-        <select id="size">
+        <select id="size" onChange={(e) => setSize(e.target.value)}>
           <option value="">All sizes</option>
           <option value="7">7</option>
           <option value="8">8</option>
           <option value="9">9</option>
         </select>
+        {size && <h2>Found {filteredProducts.length} items</h2>}
       </section>
       <section id="products">
-        {products &&
-          products.map((p) => (
+        {filteredProducts &&
+          filteredProducts.map((p) => (
             <Product key={p.id} {...p} category={category} />
           ))}
       </section>
