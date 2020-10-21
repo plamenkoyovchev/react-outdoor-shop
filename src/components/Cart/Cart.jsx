8f2ffd2814431274985928610a1c798f2aa9c0b8
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-21 10:14:29 +0300

    renderCartItem functionality initial commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
-const Cart = () => {
-  return <div>Cart</div>;
+import useFetchAll from "../../hooks/useFetchAll";
+import Spinner from "../../Spinner";
+
+const Cart = ({cart, updateQuantity}) => {
+  const urls = cart.map((i) => `products/${i.id}`);
+  const { data: products, loading, error } = useFetchAll(urls);
+
+  const renderCartItem = (item) => {
+    const {id, sku, quantity} = item;
+    const {price, name, image, skus} = products.find(
+      (p) => p.id === parseInt(id)
+    );
+    const {size} = skus.find((s) => s.sku === sku);
+  };
+
+  if(loading) {
+    return <Spinner />;
+  }
+
+  if(error){
+    throw error;
+  }
+
+  return <section id="cart">
+          <h1>Cart</h1>
+          <ul>
+            {cart.map(renderCartItem)}
+          </ul>
+        </section>;
 };
 
 export default Cart;
