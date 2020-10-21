c72b76c8e454ce1612bfa27b56926117dc74a108
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-21 10:02:53 +0300

    Implement useFetchAll custom hook

---
+++ src/hooks/useFetchAll.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useFetchAll = (urls) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const promises = urls.map((url) => 
+            fetch(process.env.REACT_APP_API_BASE_URL + url)
+                .then((response) => {
+                    if(response.ok) {
+                        return response.json();
+                    }
+
+                    throw response;
+                }));
+
+        Promise.all(promises)
+                .then((json) => setData(json))
+                .catch((e) => setError(e))
+                .finally(() => setLoading(false));
+    }, [urls]);
+
+    return {data, loading, error};
+};
+
+export default useFetchAll;
