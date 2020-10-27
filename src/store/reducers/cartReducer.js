ec7e1ce5602bd9216dbd422ffd51eec4ba92df1a
plamenyovchev <plamenkooo@gmail.com>
2020-10-27 14:35:53 +0200

    Refactor cart state to be handled in cartReducer

---
+++ src/store/reducers/cartReducer.js
@@ -0,0 +1,46 @@
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD": {
+            const { cart: items } = state;
+            const { id, sku } = action.payload;
+
+            const itemInCart = items.find((i) => i.sku === sku);
+            if (itemInCart) {
+                return {
+                    ...state,
+                    cart: items.map((i) => i.sku === sku ? { ...i, quantity: i.quantity + 1 } : i)
+                };
+            } else {
+                return {
+                    ...state,
+                    cart: [...items, { id, sku, quantity: 1 }]
+                };
+            }
+        }
+        case "UPDATE_QUANTITY": {
+            const { cart: items } = state;
+            const { sku, quantity } = action.payload;
+            if (quantity === 0) {
+                return {
+                    ...state,
+                    cart: items.filter((i) => i.sku !== sku)
+                };
+            }
+
+            const itemInCart = items.find((i) => i.sku === sku);
+            if (itemInCart) {
+                return {
+                    ...state,
+                    cart: items.map((i) => i.sku === sku ? { ...i, quantity } : i)
+                };
+            }
+
+            return state;
+        }
+        default:
+            console.warn('action.type is not found');
+            return state;
+    }
+}
+
+export default cartReducer;
