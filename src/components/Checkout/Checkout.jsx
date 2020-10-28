2348e14418528d927d45b42f61181db40203afb9
Plamen Yovchev <plamenkooo@gmail.com>
2020-10-28 14:29:03 +0200

    Add event handlers

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
+import { useState } from 'react';
+
+const initialState = {
+    country: 0,
+    city: "",
+    address: ""
+};
 
 const Checkout = () => {
+
+    const [checkoutData, setCheckoutData] = useState(initialState);
+
+    const onFormSubmitHandler = (e) => {
+        e.preventDefault();
+    }
+
+    const onChangeHandler = (e) => {
+        e.persist();
+        setCheckoutData((currentCheckoutData) => {
+            return {
+                ...currentCheckoutData,
+                [e.target.name]: e.target.value
+            };
+        });
+    }
+
+    const onBlurHandler = (e) => {
+
+    }
+
     return (
         <>
             <h2>Checkout</h2>
-            <form onSubmit={() => {}}>
+            <form onSubmit={onFormSubmitHandler}>
                 <div className="form-group">
                     <label className="form-label">Country</label>
-                    <select className="form-control">
+                    <select name="country" className="form-control" value={checkoutData.country} onChange={onChangeHandler} onBlur={onBlurHandler}>
                         <option value={0}>--Choose Country--</option>
                         <option value={1}>Bulgaria</option>
                         <option value={2}>Germany</option>
@@ -18,11 +46,11 @@ const Checkout = () => {
                 </div>
                 <div className="form-group">
                     <label className="form-label">City</label>
-                    <input name="city" className="form-control"/>
+                    <input name="city" className="form-control" value={checkoutData.city} onChange={onChangeHandler} onBlur={onBlurHandler} />
                 </div>
                 <div className="form-group">
                     <label className="form-label">Address</label>
-                    <input name="address" className="form-control"/>
+                    <input name="address" className="form-control" value={checkoutData.address} onChange={onChangeHandler} onBlur={onBlurHandler} />
                 </div>
                 <div>
                     <input type="submit" className="btn btn-primary" />
