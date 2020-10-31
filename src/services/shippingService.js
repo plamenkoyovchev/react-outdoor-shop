12d28a9a42d28f525d0c25f3074f6666515ef5cf
plamenyovchev <plamenkooo@gmail.com>
2020-10-31 16:44:59 +0200

    refactor shippingService

--- src/services/shippingService.js
+++ src/services/shippingService.js
@@ -1,14 +1,19 @@
 const baseUrl = process.env.REACT_APP_API_BASE_URL;
 
 export async function getShippingAddress(userId) {
-  return fetch(baseUrl + "shippingAddress/" + userId).then((response) => {
-    if (response.ok) return response.json();
+  const url = `${baseUrl}shippingAddress/${userId}`;
+  return fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+
     throw response;
   });
 }
 
 export async function saveShippingAddress(address) {
-  return fetch(baseUrl + "shippingAddress", {
+  const url = `${baseUrl}shippingAddress`;
+  return fetch(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
